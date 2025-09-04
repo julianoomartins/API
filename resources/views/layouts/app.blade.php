695f141049f510904876196a87854458140732af
Juliano <juliano.cmartins@hotmail.com>
2025-09-04 03:10:48 -0300

    04/09/2025

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,10 +6,11 @@
   <title>{{ $title ?? config('app.name','Laravel') }}</title>
   @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
-<body class="min-h-dvh bg-gray-50 text-gray-900 antialiased">
+<body>
   @include('partials.navbar')
-  <main class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-6">
+  <main>
     @yield('content')
   </main>
 </body>
+
 </html>
